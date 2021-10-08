1ad8f6b36291da5d94e7eb26584061df3c7ec9f6
FelixManFongCheung <manfongcheung74@gmail.com>
2021-10-08 02:45:47 +0200

    final cut

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,25 +3,43 @@ import React from "react";
 function Item({ products }) {
   return (
     <div>
-      <div className="product-name">{products.productName.toUpperCase()}</div>
-      <div className="product-type">Type: {products.productType}</div>
-      <ul className="tags">
-        {products.targetAudience.map((tag, index) => {
-          return <li key={index}>{tag[0].toUpperCase() + tag.substring(1)}</li>;
-        })}
-      </ul>
-      <div className="product-information">Product information</div>
-      <div className="product-description">{products.productInformation}</div>
-      <div className="price">{products.price} kr./md.</div>
-      <div className="button-container">
-        <button className="do-the-test" type="button">
-          Do the test
-        </button>
-        <button className="add-to-cart" type="button">
-          Add to cart
-        </button>
+      <div className="section-1">
+        <img
+          src={require("../assets/web/webVer.png").default}
+          alt="fish-oil"
+          className="fish-oil"
+        />
+        <div className="content">
+          <div className="product-name">
+            {products.productName.toUpperCase()}
+          </div>
+          <div className="product-type-container">
+            Type: <span className="product-type">{products.productType}</span>
+          </div>
+          <ul className="tags">
+            {products.targetAudience.map((tag, index) => {
+              return (
+                <li key={index}>{tag[0].toUpperCase() + tag.substring(1)}</li>
+              );
+            })}
+          </ul>
+          <div className="product-information">Product information</div>
+          <div className="product-description">
+            {products.productInformation}
+          </div>
+          <div className="price">{products.price} kr./md.</div>
+          <div className="button-container">
+            <button className="do-the-test" type="button">
+              Do the test
+            </button>
+            <button className="add-to-cart" type="button">
+              Add to cart
+            </button>
+          </div>
+        </div>
       </div>
-      <ul className="badges">
+
+      <ul className="section-2">
         {Object.keys(products.badges).map((key, index) => {
           console.log(products.badges[key].title);
           return (
